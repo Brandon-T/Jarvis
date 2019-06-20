87d47743e88373e661121a84d0a6676de3c82261
BrandonT <Brandon@plasticmobile.com>
2019-06-20 17:41:15 -0400

    Updated LogDetailsViewCOntroller

--- PromiseClient/RequestLogDetailsViewController.swift
+++ PromiseClient/RequestLogDetailsViewController.swift
@@ -8,15 +8,24 @@
 
 import Foundation
 import UIKit
+import Jarvis
 
 class RequestLogDetailsViewController: UIViewController {
     private let tabBar = TabBar(tabTitles: ["Request", "Response"])
     private let infoView = UITextView(frame: .zero)
     
+    var requestLog: RequestLogger.RequestPacket? {
+        didSet {
+            updateInfoView(index: 0)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        infoView.showsVerticalScrollIndicator = true
+        infoView.isEditable = false
         
         view.addSubview(tabBar)
         view.addSubview(infoView)
@@ -37,7 +46,154 @@ class RequestLogDetailsViewController: UIViewController {
         
         tabBar.addEventHandler(runnable: { [weak self] index in
             self?.tabBar.enableTabs()
+            
+            self?.infoView.text = ""
+            self?.updateInfoView(index: index)
         })
+    }
+    
+    private func attributedText(_ text: String, weight: UIFont.Weight, size: CGFloat, colour: UIColor) -> NSAttributedString {
+        return NSAttributedString(string: text, attributes: [
+            .font: UIFont.systemFont(ofSize: size, weight: weight),
+            .foregroundColor: colour
+        ])
+    }
+    
+    private func updateInfoView(index: Int) {
+        guard let requestLog = requestLog else {
+            return
+        }
+        
+        if index == 0 {
+            let startDate = requestLog.startDate
+            let components = Calendar.current.dateComponents([.second, .minute, .hour], from: startDate)
+            let date = String(format: "[%02ld:%02ld:%02ld]", components.hour!, components.minute!, components.second!) //swiftlint:disable:this force_unwrapping
+            
+            let logData = NSMutableAttributedString(string: "", attributes: [
+                .font: UIFont.systemFont(ofSize: 12.0, weight: .medium),
+                .foregroundColor: UIColor.lightGray
+            ])
+            
+            // Normalize host
+            var host = requestLog.request.url?.host ?? ""
+            host = host.isEmpty ? "localhost" : host
+            
+            // Normalize path
+            var path = requestLog.request.url?.path ?? ""
+            path = path.isEmpty ? "/" : path
+            
+            // Normalize query
+            let query = requestLog.request.url?.query ?? ""
+            
+            // Normalize headers
+            var requestHeaders = requestLog.request.allHTTPHeaderFields ?? [:]
+            requestHeaders.removeValue(forKey: "Host")
+            
+            logData.append(attributedText("\(date) ", weight: .bold, size: 12.0, colour: .orange))
+            logData.append(attributedText("\(requestLog.request.httpMethod?.uppercased() ?? "GET") ", weight: .bold, size: 12.0, colour: .black))
+            logData.append(attributedText("\(path)\n", weight: .bold, size: 12.0, colour: .black))
+            
+            logData.append(attributedText("Host: ", weight: .bold, size: 12.0, colour: .orange))
+            logData.append(attributedText("\(host)\n", weight: .medium, size: 12.0, colour: .black))
+            
+            if !query.isEmpty {
+                logData.append(attributedText("Query: ", weight: .bold, size: 12.0, colour: .orange))
+                logData.append(attributedText("\(query)\n", weight: .medium, size: 12.0, colour: .black))
+            }
+            
+            logData.append(attributedText("\n", weight: .medium, size: 12.0, colour: .black))
+            
+            logData.append(attributedText("Headers:\n", weight: .bold, size: 12.0, colour: .orange))
+            logData.append(attributedText("\(indent(requestHeaders.compactMap({ "\($0.key): \($0.value)" }).joined(separator: "\n")))\n\n", weight: .medium, size: 12.0, colour: .black))
+            
+            let body = String(data: requestLog.request.httpBody ?? Data(), encoding: .utf8) ?? ""
+            if !body.isEmpty {
+                logData.append(attributedText("Body:\n", weight: .bold, size: 12.0, colour: .orange))
+                logData.append(attributedText("\(indent(body))\n\n", weight: .medium, size: 12.0, colour: .black))
+            }
+        }
+        else {
+            let startDate = requestLog.startDate
+            let endDate = requestLog.endDate
+            let timeInterval = endDate.timeIntervalSince(startDate)
+            
+            let components = Calendar.current.dateComponents([.second, .minute, .hour], from: startDate)
+            let date = String(format: "[%02ld:%02ld:%02ld]", components.hour!, components.minute!, components.second!) //swiftlint:disable:this force_unwrapping
+            
+            let logData = NSMutableAttributedString(string: "", attributes: [
+                .font: UIFont.systemFont(ofSize: 12.0, weight: .medium),
+                .foregroundColor: UIColor.lightGray
+            ])
+            
+            // Normalize host
+            var host = requestLog.request.url?.host ?? ""
+            host = host.isEmpty ? "localhost" : host
+            
+            // Normalize path
+            var path = requestLog.request.url?.path ?? ""
+            path = path.isEmpty ? "/" : path
+            
+            // Normalize query
+            let query = requestLog.request.url?.query ?? ""
+            
+            // Normalize headers
+            var responseHeaders = requestLog.response.allHeaderFields
+            responseHeaders.removeValue(forKey: "Host")
+            
+            // Normalize body
+            let contentType = requestLog.response.allHeaderFields["Content-Type"] as? String ?? "application/octet-stream"
+            
+            logData.append(attributedText("\(date) ", weight: .bold, size: 12.0, colour: .orange))
+            logData.append(attributedText("\(requestLog.request.httpMethod?.uppercased() ?? "GET") ", weight: .bold, size: 12.0, colour: .black))
+            logData.append(attributedText("\(path)\n", weight: .bold, size: 12.0, colour: .black))
+            
+            logData.append(attributedText("Host: ", weight: .bold, size: 12.0, colour: .orange))
+            logData.append(attributedText("\(host)\n", weight: .medium, size: 12.0, colour: .black))
+            
+            logData.append(attributedText("\n", weight: .medium, size: 12.0, colour: .black))
+            
+            // Log Status Code
+            if let error = requestLog.error as? URLError, error.code == .cancelled {
+                logData.append(attributedText("Status: ", weight: .bold, size: 12.0, colour: .orange))
+                logData.append(attributedText("\(requestLog.response.statusCode) CANCELLED\n\n", weight: .medium, size: 12.0, colour: .black))
+            }
+            else {
+                logData.append(attributedText("Status: ", weight: .bold, size: 12.0, colour: .orange))
+                logData.append(attributedText("\(requestLog.response.statusCode) \(requestLog.response.statusCode == 200 ? "OK" : HTTPURLResponse.localizedString(forStatusCode: requestLog.response.statusCode).uppercased())\n\n", weight: .medium, size: 12.0, colour: .black))
+            }
+            
+            logData.append(attributedText("Elapsed Time: ", weight: .bold, size: 12.0, colour: .orange))
+            logData.append(attributedText("\(String(format: "%.3fs", timeInterval))\n", weight: .medium, size: 12.0, colour: .black))
+            
+            logData.append(attributedText("Headers:\n", weight: .bold, size: 12.0, colour: .orange))
+            logData.append(attributedText("\(indent(responseHeaders.compactMap({ "\($0.key): \($0.value)" }).joined(separator: "\n")))\n\n", weight: .medium, size: 12.0, colour: .black))
+            
+            let body = requestLog.responseData ?? Data()
+            if !body.isEmpty {
+                var bodyData = ""
+                if contentType.starts(with: "text") {
+                    let decodedBody = String(data: body, encoding: .utf8) ?? String(data: body, encoding: .ascii) ?? ""
+                    bodyData += decodedBody.isEmpty ? "" : indent(decodedBody)
+                }
+                else if contentType.starts(with: "image") {
+                    bodyData += indent(body.base64EncodedString())
+                }
+                else if contentType.starts(with: "application/octet-stream") {
+                    bodyData += indent(body.base64EncodedString())
+                }
+                else {
+                    bodyData += indent(body.base64EncodedString())
+                }
+                
+                logData.append(attributedText("Body:\n", weight: .bold, size: 12.0, colour: .orange))
+                logData.append(attributedText("\(bodyData)\n\n", weight: .medium, size: 12.0, colour: .black))
+            }
+        }
+    }
+    
+    private func indent(_ input: String, amount: Int = 4) -> String {
+        let indentation = String(repeating: " ", count: amount)
+        return "\(indentation)\(input.components(separatedBy: "\n").joined(separator: "\n\(indentation)"))"
     }
 }
 
